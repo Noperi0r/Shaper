3b789714ced11d8ace88e1a24ecd312859c37b19
SYS <39945608+tladudtn@users.noreply.github.com>
2023-11-08 08:04:33 +0900

    LevelClass Edit & Add Patterns
    aa

--- LevelClass.py
+++ LevelClass.py
@@ -37,7 +37,7 @@ class LevelManager():
         screen.blit(text, (text_x, text_y))
         
         start_font = pygame.font.Font(None, 36)  # 다른 글꼴 및 크기 설정
-        start_text = start_font.render("Press SPACE to start", True, (255, 255, 255))
+        start_text = start_font.render("press [SPACE] to start", True, (255, 255, 255))
         start_x = center_x - (start_text.get_width() // 2)
         start_y = text_y + text.get_height() + 20  # "SHARPER" 텍스트 아래에 추가 간격
         screen.blit(start_text, (start_x, start_y))
@@ -68,8 +68,8 @@ class LevelManager():
 
         # 점수 표시
         score_font = pygame.font.Font(None, 36)  # 기본 글꼴, 글꼴 크기 72
-        score_text = score_font.render(f" Score : {round(score, 2)}", True, (255, 255, 255))
-        screen.blit(score_text, (start_x, start_y+100))
+        score_text = score_font.render(f" SCORE : {round(score, 2)}", True, (255, 255, 255))
+        screen.blit(score_text, (start_x+75, start_y+100))
         
 
         self.isStage1 = False
@@ -84,29 +84,28 @@ class LevelManager():
         
 
         # 폰트 및 크기 선택
-        text_font = pygame.font.Font(None, 36) 
-
+        text_font = pygame.font.Font(None,72) 
+        text_font1 = pygame.font.Font(None,48) 
         # 텍스트, 안티앨리어싱 적용, 글자색 지정
         text1 = text_font.render("[ Lv. 1 ]", True, (255, 255, 255))  
         text2 = text_font.render("[ Lv. 2 ]", True, (255, 255, 255))  
         text3 = text_font.render("[ Lv. 3 ]", True, (255, 255, 255))  
-        text4 = text_font.render("[ Lv. 4 ]", True, (255, 255, 255))  
+        text4 = text_font1.render("[ PRESS 1 ~ 3 ]", True, (255, 255, 255))  
         rect1 = text1.get_rect()
         rect2 = text2.get_rect()
         rect3 = text3.get_rect()
         rect4 = text4.get_rect()
 
         # 버튼 위치 조정
-        rect1.topleft = (125, 280)  
-        rect2.topleft = (275, 280)  
-        rect3.topleft = (425, 280)  
-        rect4.topleft = (575, 280) 
-        
+        rect1.topleft = (125, 325)  
+        rect2.topleft = (450, 325)  
+        rect3.topleft = (725, 325)  
+        rect4.topleft = (420, 500)
         screen.blit(text1, rect1)
         screen.blit(text2, rect2)
         screen.blit(text3, rect3)
         screen.blit(text4, rect4)
-
+        
         pygame.display.flip()
     
 
